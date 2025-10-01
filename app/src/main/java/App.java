568dd8a459d3fb5b4b7072c0674cb0eb99c17b16
Hyun Kang <107344451+HyunWrites0721@users.noreply.github.com>
2025-10-01 12:18:07 +0900

    Revert "Revert "한 줄 삭제 기능 구현 추가""

--- app/src/main/java/App.java
+++ app/src/main/java/App.java
@@ -1,5 +1,5 @@
 import javax.swing.*;
-
+import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
@@ -13,8 +13,23 @@ public class App {
         FrameBoard frame = new FrameBoard();
 
         frame.setVisible(true);
-        
 
+        Scanner sc = new Scanner(System.in);
+        
+        while(true) {
+
+            System.out.print("Enter row and column to set block text (e.g., '5 3'): ");
+            int row = sc.nextInt();
+            int col = sc.nextInt();
+            frame.setBlockText(row, col);  // 입력받은 위치에 "" 표시
+            
+
+            for (int i = 0; i < 20; i++) {
+
+                frame.oneLineClear(i);
+
+            }
+        }
 
     }
     
